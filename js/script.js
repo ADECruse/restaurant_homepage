f5314d7ff74290f680588693fb9eee7ffcd090e4
Alan Cruse <alancruse86@gmail.com>
2017-09-23 20:25:18 +0200

    Add elements with JS

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function main() {
+  $('#content').append("<h1>Julias Pasta Palace</h1>", "<p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>", "<img src='images/pasta.jpeg' alt='pasta'>");
+}
+
+$(document).ready(main);
