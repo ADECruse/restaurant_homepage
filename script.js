5a3a4857a7a749945b8c4b7ea7afd26962ee8a4d
Alan Cruse <alancruse86@gmail.com>
2017-10-01 16:35:10 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const home = "<h1>Julia's Pasta Palace</h1><img src='images/pasta.jpeg' alt='pasta'><p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>"
+const home = "<h1>Julia's Pasta Palace</h1><img src='pasta.jpeg' alt='pasta'><p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>"
 const menu = "<p>Chicken and Mushroom Cream Spaghetti</p><p>Gnocchi</p><p>Tomato Penne</p>"
 const contact = "<p>Julia Sommer</p> <p>81 Tasty street</p> <p>Nudelberg</p> <p>Germany</p> <p>11-111-111-111</p>"
 function main() {
