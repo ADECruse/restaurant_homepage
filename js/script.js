ed2539f81453d3086d75b3d7e5ced1528d92135a
Alan Cruse <alancruse86@gmail.com>
2017-10-01 16:30:48 +0200

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
+const home = "<h1>Julia's Pasta Palace</h1><img src='images/pasta.jpeg' alt='pasta'><p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>"
+const menu = "<p>Chicken and Mushroom Cream Spaghetti</p><p>Gnocchi</p><p>Tomato Penne</p>"
+const contact = "<p>Julia Sommer</p> <p>81 Tasty street</p> <p>Nudelberg</p> <p>Germany</p> <p>11-111-111-111</p>"
 function main() {
-  $('#content').append("<h1>Julia's Pasta Palace</h1><img src='images/pasta.jpeg' alt='pasta'><p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>");
+  $('#content').append(home);
 
   $("a[href='#home']").on('click', function(){
     $('a').removeClass("active");
     $(this).addClass("active");
     $('#content').empty();
-    $('#content').append("<h1>Julia's Pasta Palace</h1><img src='images/pasta.jpeg' alt='pasta'><p>Julia loves Pasta and you will love her Pasta too! Come eat at the Pasta Palace today!</p>")
+    $('#content').append(home)
   });
 
   $("a[href='#contact']").on('click', function(){
     $('a').removeClass("active");
     $(this).addClass("active");
     $('#content').empty();
-    $('#content').append("<p>Julia Sommer</p> <p>81 Tasty street</p> <p>Nudelberg</p> <p>Germany</p> <p>11-111-111-111</p>")
+    $('#content').append(contact)
   });
 
   $("a[href='#menu']").on('click', function(){
     $('a').removeClass("active");
     $(this).addClass("active");
     $('#content').empty();
-    $('#content').append("<p>chicken and mushroom cream spaghetti</p><p>gnocchi</p><p>tomato penne</p>")
+    $('#content').append(menu)
   });
 }
 
